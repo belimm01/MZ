a0da879c6b095b1efd75dab6e109478b60c73e18
Mykyta Belimenko <nikita.belimenko@enehano.cz>
2020-02-21 10:30:17 +0100

    broken toogle

---
+++ savage/src/components/ProcessState.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <b-button v-b-toggle.collapse-1 variant="primary">Toggle Collapse</b-button>
+        <b-collapse id="collapse-1" class="mt-2">
+            <b-card>
+                <p class="card-text">Collapse contents Here</p>
+                <b-button v-b-toggle.collapse-1-inner size="sm">Toggle Inner Collapse</b-button>
+                <b-collapse id="collapse-1-inner" class="mt-2">
+                    <b-card>Hello!</b-card>
+                </b-collapse>
+            </b-card>
+        </b-collapse>
+    </div>
+</template>
+
+<script>
+    import {BButton, BCard, BCollapse} from "bootstrap-vue/esm";
+
+    export default {
+        name: "ProcessState",
+        props: ['isHidden', 'currentUser'],
+        components: {
+            'b-button': BButton,
+            'b-collapse': BCollapse,
+            'b-card': BCard,
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+    .divider {
+        width: 5px;
+        height: auto;
+        display: inline-block;
+    }
+</style>
--- savage/src/pages/UserAccreditationInfo.vue
+++ savage/src/pages/UserAccreditationInfo.vue
@@ -3,11 +3,12 @@
         <div>
             <b-card>
                 <b-card alt="User avatar" :img-src="require('../assets/user-icon.svg')" img-right>
+                    <ActionButton :currentUser="currentUser" :isHidden="false"/>
                     <b-card-text>
                         <h1 class="text-center">User information</h1>
                         <b-table stacked :items="currentUser.info"></b-table>
                     </b-card-text>
-                    <ActionButton :currentUser="currentUser" :isHidden="false"/>
+                    <ProcessState :currentUser="currentUser"></ProcessState>
                 </b-card>
             </b-card>
         </div>
@@ -17,11 +18,13 @@
 <script>
     import {BTable, BCard, BCardText} from "bootstrap-vue/esm";
     import ActionButton from "../components/ActionButton";
+    import ProcessState from "../components/ProcessState";
 
     export default {
         name: "UserAccreditationInfo",
         props:['currentUser'],
         components: {
+            ProcessState,
             ActionButton,
             'b-card': BCard,
             'b-card-text': BCardText,
